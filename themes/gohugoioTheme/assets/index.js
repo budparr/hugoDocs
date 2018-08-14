3024f880d7ae4f8ee746ef0e7b8a805b44aaace2
budparr <budparr@gmail.com>
2018-08-14 09:15:45 -0400

    Use a separate entry point for Webpack-based assets

---
+++ themes/gohugoioTheme/assets/index.js
@@ -0,0 +1,11 @@
+require("typeface-muli")
+import './js/anchorforid.js'
+import './js/clipboardjs.js'
+import './js/codeblocks.js'
+import './js/docsearch.js'
+import './js/lazysizes.js'
+import './js/menutoggle.js'
+import './js/scrolldir.js'
+import './js/smoothscroll.js'
+import './js/tabs.js'
+import './js/nojs.js'
